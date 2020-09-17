d415ef90c84bf553a7dc941d946d773eb6487488
Katherine Hambley <superdesigngirl@mac.com>
2020-09-17 11:25:10 -0500

    Added aspnet core backend api project files

--- frontend/src/app/api.service.ts
+++ frontend/src/app/api.service.ts
@@ -1,5 +1,14 @@
 import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
+@Injectable()
 export class ApiService {
 
+  constructor(private http: HttpClient){}
+
+  postQuestion(question) {  //Add Url once we get it, have to create endpoint in ASP.NET Core
+    this.http.post('', question).subscribe(res =>{
+      console.log(res)
+    })
+  }
 }
