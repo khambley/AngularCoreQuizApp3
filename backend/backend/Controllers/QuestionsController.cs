3cecb2a99623d21d2f3b89fe4f1f87630f6cc021
Katherine Hambley <superdesigngirl@mac.com>
2020-09-17 11:47:42 -0500

    Added QuestionsController and Post method

---
+++ backend/backend/Controllers/QuestionsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase 
+    {
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+
+        }
+    
+    }
+    
+
+    
+}
