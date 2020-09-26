ee2b75cb5f89eca043a98db56fe0176ec917b7c7
Katherine Hambley <khambley@franklinenergy.com>
2020-09-25 19:07:32 -0500

    Added Register form in Angular frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,12 +15,13 @@ import { HomeComponent } from './home.component'
 import { NavComponent } from './nav.component'
 import { QuizComponent } from './quiz.component'
 import { QuizzesComponent } from './quizzes.component'
+import { RegisterComponent } from './register.component'
 
 const routes = [
   { path: '', component: HomeComponent },
   { path: 'question', component: QuestionComponent },
   { path: 'question/:quizId', component: QuestionComponent },
-  { path: 'questions', component: QuestionsComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'quiz', component: QuizComponent }
 ]
   
@@ -28,7 +29,7 @@ const routes = [
 
 @NgModule({
   declarations: [
-    AppComponent, QuestionComponent, QuestionsComponent, HomeComponent, NavComponent, QuizComponent, QuizzesComponent
+    AppComponent, QuestionComponent, QuestionsComponent, HomeComponent, NavComponent, QuizComponent, QuizzesComponent, RegisterComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/nav.component.ts
+++ frontend/src/app/nav.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'nav',
   template: `
     <mat-toolbar>
         <button mat-button routerLink="/">Quiz</button>
-        <button mat-button routerLink="/question">Question</button>
-        <button mat-button routerLink="/questions">Question List</button>
+        <span style="flex: 1 1 auto;"></span>
+        <button mat-button routerLink="/register">Register</button>
     </mat-toolbar>
   `
 })
